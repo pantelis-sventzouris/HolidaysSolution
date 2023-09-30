241a079cffa3556b859a221469a0fa6c0807e4ac
Pantelis Sventzouris <dev@psventzouris.eu>
2023-09-30 17:06:54 +0300

    Add project files.

---
+++ TestConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var lst = HolidaysLib.HolidaysLib.GetGreekHolidays(DateTime.Now.Year, true, true);
+
+            foreach (var item in lst)
+            {
+                Console.WriteLine(item.Key.ToLongDateString() + ": " + item.Value);
+            }
+            Console.ReadKey();
+        }
+    }
+}
